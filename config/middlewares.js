486da8e0b63b78c3e5e675ab97dcf0c6fc4cc55f
Cris1816 <dev_02@satech.mx>
2023-11-28 12:41:00 -0600

    CORS Allow Any Origin

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,7 +7,7 @@ module.exports = [
     config: {
       enabled: true,
       headers: '*',
-      origin: ['http://localhost:1337', 'http://example2']
+      origin: '*'
     }
   },
   'strapi::logger',
